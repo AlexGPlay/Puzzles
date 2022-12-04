390c13adda01051db8db20adf5e4af1be94c904c
alexgplay <alexgp1234@gmail.com>
2022-12-04 12:33:41 +0100

    advent of code day 4

---
+++ advent of code/adventofcode.com/2022/04/challenge04.rb
@@ -0,0 +1,21 @@
+file_data = File.open('input.txt').to_a
+
+# Part 1
+puts(
+  file_data.count do |entry|
+    range1, range2 = entry.split(',').map{|r| (r.split('-')[0].to_i..r.split('-')[1].to_i)}
+    range1.cover?(range2) || range2.cover?(range1)
+  end
+)
+
+# Part 2
+def overlaps?(range_a, range_b)
+  range_b.begin <= range_a.end && range_a.begin <= range_b.end 
+end 
+
+puts(
+  file_data.count do |entry|
+    range1, range2 = entry.split(',').map{|r| (r.split('-')[0].to_i..r.split('-')[1].to_i)}
+    overlaps?(range1, range2) || overlaps?(range2, range1)
+  end
+)
