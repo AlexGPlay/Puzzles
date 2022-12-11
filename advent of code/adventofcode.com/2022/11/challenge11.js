2287aca3d09751b9b7c12726d000a826949b5005
alexgplay <alexgp1234@gmail.com>
2022-12-11 14:39:43 +0100

    Simplify code

--- advent of code/adventofcode.com/2022/11/challenge11.js
+++ advent of code/adventofcode.com/2022/11/challenge11.js
@@ -6,7 +6,6 @@ const monkeys = fs
   .split("\n\n")
   .map((monkeyData) => {
     const [_, items, operation, test, testTrue, testFalse] = monkeyData.split("\n");
-    console.log("");
     return {
       items: items.split(":")[1].split(",").map(Number),
       operation: (currentData) => eval(operation.split("=")[1].replaceAll("old", currentData)),
@@ -21,17 +20,13 @@ const monkeys = fs
 const gcd = (a, b) => (a ? gcd(b % a, a) : b);
 const lcm = (a, b) => (a * b) / gcd(a, b);
 
-function play(rounds, monkeys, divideWorryLevel, moduloWorryLevel = null) {
+function play(rounds, monkeys, worryFn) {
   for (let i = 0; i < rounds; i++) {
-    if ((i + 1) % 1000 === 0) {
-      console.log(i + 1, "/", rounds);
-    }
     for (let monkey of monkeys) {
       for (let item of [...monkey.items]) {
         monkey.inspectedItems += 1;
         let worryLevel = monkey.operation(item);
-        if (divideWorryLevel) worryLevel = Math.floor(worryLevel / 3);
-        if (moduloWorryLevel) worryLevel %= moduloWorryLevel;
+        worryLevel = worryFn(worryLevel);
         const nextMonkey = monkey.test(worryLevel) ? monkey.onTestTrue : monkey.onTestFalse;
         monkey.items.splice(0, 1);
         monkeys[nextMonkey].items.push(worryLevel);
@@ -49,7 +44,7 @@ console.log(
   play(
     PART_1_ROUNDS,
     monkeys.map((monkey) => ({ ...monkey, items: [...monkey.items] })),
-    true
+    (worryLevel) => Math.floor(worryLevel / 3)
   )
 ); // 151312
 
@@ -59,7 +54,6 @@ console.log(
   play(
     PART_2_ROUNDS,
     monkeys.map((monkey) => ({ ...monkey, items: [...monkey.items] })),
-    false,
-    monkeys.map((monkey) => monkey.testNumber).reduce(lcm)
+    (worryLevel) => worryLevel % monkeys.map((monkey) => monkey.testNumber).reduce(lcm)
   )
 );
