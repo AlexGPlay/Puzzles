288480d28f048c04ca9e422870a0fa8c70fc64f5
alexgplay <alexgp1234@gmail.com>
2023-12-19 23:01:14 +0100

    Add part 2

--- advent of code/adventofcode.com/2023/19/challenge19.py
+++ advent of code/adventofcode.com/2023/19/challenge19.py
@@ -4,7 +4,7 @@ import re
 rules = {}
 ratings = []
 
-with open("example.txt") as file:
+with open("input.txt") as file:
     possible_rules, possible_ratings = file.read().split("\n\n")
 
     for rule in possible_rules.split("\n"):
@@ -58,8 +58,69 @@ def evaluate_rating(rating):
     return 0
 
 
+def split_rule_by_workflow(rule, workflow):
+    split = []
+    copy_rule = rule.copy()
+
+    for condition, new_workflow_key in workflow:
+        if condition == "True":
+            split.append((copy_rule, new_workflow_key))
+            continue
+
+        results = re.search(r"(?P<name>\w+)(?P<operation><|>)(?P<value>\d+)", condition)
+        variable_name = results.group("name")
+        operation = results.group("operation")
+        value = int(results.group("value"))
+
+        if operation == "<":
+            new_rule = copy_rule.copy()
+            new_rule[variable_name] = (copy_rule[variable_name][0], value - 1)
+            copy_rule[variable_name] = (value, copy_rule[variable_name][1])
+            split.append((new_rule, new_workflow_key))
+        elif operation == ">":
+            new_rule = copy_rule.copy()
+            new_rule[variable_name] = (value + 1, copy_rule[variable_name][1])
+            copy_rule[variable_name] = (copy_rule[variable_name][0], value)
+            split.append((new_rule, new_workflow_key))
+
+    return split
+
+
 # Part 1
 result = 0
 for rating in ratings:
     result += evaluate_rating(rating)
 print(result)
+
+# Part 2
+part_2_base_rule = {
+    "x": (1, 4000),
+    "m": (1, 4000),
+    "a": (1, 4000),
+    "s": (1, 4000),
+}
+
+pending_rules = [(part_2_base_rule, "in")]
+results = []
+
+while len(pending_rules) > 0:
+    rule, workflow_key = pending_rules.pop(0)
+    new_rules = split_rule_by_workflow(rule, rules[workflow_key])
+
+    for new_rule, new_workflow_key in new_rules:
+        if new_workflow_key == "A":
+            results.append(new_rule)
+            continue
+        elif new_workflow_key == "R":
+            continue
+
+        pending_rules.append((new_rule, new_workflow_key))
+
+total_results = 0
+for result in results:
+    part = 1
+    for value in result.values():
+        part *= value[1] - value[0] + 1
+    total_results += part
+
+print(total_results)
