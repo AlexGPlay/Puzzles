ba54c0c3df45f3e144cfc6593bd0a6ab85da4217
AlexGPlay <alexgp1234@gmail.com>
2024-12-01 13:34:11 +0100

    AoC day 1

---
+++ advent of code/adventofcode.com/2024/01/01.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+)
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func part1() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	var column1 []int
+	var column2 []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		var value1, value2 int
+
+		fmt.Sscanf(line, "%d %d", &value1, &value2)
+		column1 = append(column1, value1)
+		column2 = append(column2, value2)
+	}
+
+	sort.IntSlice(column1).Sort()
+	sort.IntSlice(column2).Sort()
+
+	total := 0
+	for i, value1 := range column1 {
+		result := abs(value1 - column2[i])
+		total += result
+	}
+
+	fmt.Println(total)
+}
+
+func part2(){
+	var column1 []int
+	columnRepetitions:= make(map[int]int)
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		var value1, value2 int
+
+		fmt.Sscanf(line, "%d %d", &value1, &value2)
+		column1 = append(column1, value1)
+
+		if _, ok := columnRepetitions[value2]; ok {
+			columnRepetitions[value2]++
+		} else {
+			columnRepetitions[value2] = 1
+		}
+	}
+
+	total := 0
+	for _, value1 := range column1 {
+		repetitions, ok := columnRepetitions[value1]
+		if ok {
+			total += repetitions * value1
+		}
+	}
+	fmt.Println(total)
+}
+
+func main(){
+	part1()
+	part2()
+}
